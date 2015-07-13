624fb6299f3f48e540565531b6dd5d274f001d84
owen <myartame@gmail.com>
2015-07-14 00:05:27 +0900

    apply url_parameter change of input value

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,39 @@
 $(document).ready(function(){
+	$('.api-box .urlparameter input[type="text"]').focusout(function(){
+		var _this = $(this)
+		var input_index = _this.parents('.urlparameter').find('input[type="text"]').index(_this)
+		var url_val = _this.parents('.group').siblings('.endpoint')
+		var method_name = _this.parents('.api-box').siblings('.method-name').text()
+
+		var split_url = url_val.text().split(method_name + '/')
+		split_url[0] += method_name
+		split_url[1].split('/').forEach(function(element, index){
+			split_url[0] += '/' + (index == input_index ? (_this.val() != '' ? _this.val() : _this.parent().siblings('div').text()) : element)
+		})
+
+		url_val.text(split_url[0])
+	})
+
 	$('button[type="submit"]').click(function(){
-		ajax_get($(this), 'localhost://method_name');
+		var api_parent = $(this).parents('section')
+
+		var ajax_config = {
+			'url' : function(){
+				var url = 'test'
+
+				$.each(api_parent.children('.parameter').children('.row'), function(index, row_val){
+					$.each($(row_val).children('div'), function(div_index, div_val){
+						var input_dom = $(div_val).children('input[type="text"]')
+						var parameter_val = input_dom.val()
+						if (parameter_val != undefined){
+							url += (index ? '&' : '?') + input_dom.parent().siblings('div').text() + '=' + parameter_val
+						}
+					})
+				})
+				console.log(url)
+			}()
+		}
+		
+		$.ajax(ajax_config)		
 	})
 })
-
-function ajax_get(button, url){
-	var api_parent = button.parents('section')
-	
-	$.ajax({
-		'url': function(){
-			$.each(api_parent.children('.parameter').children('.row'), function(index, row_val){
-				$.each($(row_val).children('div'), function(div_index, div_val){
-					var input_dom = $(div_val).children('input[type="text"]')
-					var parameter_val = input_dom.val()
-					if (parameter_val != undefined){
-						url += (index ? '&' : '?') + input_dom.parent().siblings('div').text() + '=' + parameter_val
-					}
-				})
-			})
-			return url
-		}(),
-	})
-}
