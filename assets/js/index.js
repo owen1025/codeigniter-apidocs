41e02012ed67d10fd2fa9f7fe22a1f8527f2f0c3
owen <myartame@gmail.com>
2015-07-18 20:45:44 +0900

    i'm so sorry

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,35 @@
+const SCROLL_Y_CONTROL = 50
+
 $(document).ready(function(){
+	$('.highlight ol li').click(function(event){
+		event.preventDefault()
+
+		var _this = $(this);
+		$('html, body').stop(true).animate({
+			scrollTop: $('.' + _this.text()).offset().top - SCROLL_Y_CONTROL
+		}, 500);
+
+		$('.highlight ol .highlight').removeClass('highlight')
+		_this.addClass('highlight')
+	})
+
+	$('li.collection').click(function(){
+		var _this = $(this);
+
+		if (_this.attr('class').indexOf('highlight') <= -1){
+			$('li.collection.highlight').removeClass('highlight')
+			_this.addClass('highlight')
+
+			$.ajax({
+				url : document.URL + '/get_api_detail/' + _this.find('label a').text(),
+				type : 'GET',
+				success : function(data){
+					json_data_convert_tag(data)
+				}
+			})
+		}
+	})
+
 	$('.api-box .urlparameter input[type="text"]').focusout(function(){
 		var _this = $(this)
 		var input_index = _this.parents('.urlparameter').find('input[type="text"]').index(_this)
@@ -47,12 +78,33 @@ $(document).ready(function(){
 
 		if (call_type != 'GET'){
 			ajax_config['data'] = ajax_data_binding(api_parent, 'parameter')
-			ajax_config['header'] = ajax_data_binding(api_parent, 'header')
+			ajax_config['headers'] = ajax_data_binding(api_parent, 'header')
 		}
 		
 		$.ajax(ajax_config)		
 	})
 })
+
+function json_data_convert_tag(controller_name, data){
+	var api_data = JSON.parse(data)
+	var tag_str = ''
+	// api_data.item.forEach(function(element, index){
+	// 	tag_str += '<div class="api-wrap ' + element.method_name + '">' +
+	// 		'<h2 class="method-name">' + element.method_name + '</h2>' +
+	// 		'<blockquote>Vestibulum rutrum quam vitae fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae. Donec sagittis faucibus lacus eget blandit. Mauris vitae ultricies metus, at condimentum nulla.</blockquote>' +
+	// 		'<section class="api-box">' +
+	// 			'<div class="method" data-method="element.call_type.toUpperCase()' + '"></div>' +
+	// 			'<div class="endpoint">';
+
+	// 	var api_url = ''
+	// 	element.url_parameter.forEach(function(urlparameter_item, urlparameter_index){
+	// 		api_url += '/{' + urlparameter_item + '}'
+	// 	})
+
+	// 	tag_str += api_data.base_url + '/' + controller_name + api_url
+	// })
+	console.log(tag_str)
+}
 
 function ajax_data_binding(api_parent, group_name){
 	var data_str = {}
