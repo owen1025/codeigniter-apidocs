8172608b1b15ab9cbf8be6df743d8b2295c2f4cc
owen <myartame@gmail.com>
2015-08-01 01:10:23 +0900

    temp

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,42 +1,22 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
 
 class Test extends CI_Controller{
-	private function _test1(){
-		echo 123;
+	function __construct(){
+		parent::__construct();
 	}
 
-	public function index(){
-		echo 123;
+	function dafslkj($t1, $t2){
+		echo $this->input->get('dasf');
 	}
 
-	public function series($test, $t_dk){
-	/* 
-	@Description blah blah blah
-	*/
-		echo xmlrpc_encode(array(
-			'test' => $test,
-			't_dk' => $t_dk,
-			'get_test' => $this->input->get('get_test')
-		));
+	function adsf( $t1, $t2,$t3 ){
+		echo $this->input->post ( 'dasf');
 	}
 
-	private function _test2(){
-		echo 234;
-	}
-
-	public function series_list(){
-		echo json_encode(array(
-			'test' => $this->input->post('test'),
-			'test2' => $this->input->post('test2'),
-			'header_test1' => $this-> input -> get_request_header( 'header_test1'),
-			'header_test2' => $this-> input -> get_request_header( 'header_test2'),
-		));
-	}
-
-	public function test3 ( $asfdlkj, $adfs){
-		echo $this->get('1234');
-		echo $this-> input -> get_request_header( 'header_test3');
-		echo $this -> input -> get('test');
+	function adfskl(){
+		if (true){
+			echo $this-> input->post('adflkj');
+		}
 	}
 }
 
